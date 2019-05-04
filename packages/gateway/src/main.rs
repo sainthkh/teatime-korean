5afb6520a62d21a25145d6210220939b31fdec4c
sainthkh <sainthkh@naver.com>
2019-05-04 19:41:26 +0900

    Added basic GraphQL server schema and related code.

--- packages/gateway/src/main.rs
+++ packages/gateway/src/main.rs
@@ -1,13 +1,109 @@
-use actix_web::{web, App, Responder, HttpServer};
+use std::io;
+use std::sync::Arc;
 
-fn index(info: web::Path<(String, u32)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.0, info.1)
+//#[macro_use]
+extern crate juniper;
+
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
+use futures::future::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+
+use juniper_from_schema::graphql_schema_from_file;
+
+// This is the important line
+graphql_schema_from_file!("src/schema.gql");
+
+pub struct Context;
+impl juniper::Context for Context {}
+
+pub struct Query;
+
+impl QueryFields for Query {
+    fn field_dummy(
+        &self,
+        _executor: &juniper::Executor<'_, Context>
+    ) -> juniper::FieldResult<bool> {
+        Ok(true)
+    }
 }
 
-fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(
-        web::resource("/{name}/{id}/index.html").to(index))
-    )
-        .bind("127.0.0.1:8080")?
-        .run()
+pub struct Mutation;
+
+impl MutationFields for Mutation {
+    fn field_signup(
+        &self, 
+        _executor: &juniper::Executor<'_, Context>,
+        _trail: &QueryTrail<'_, SignupResponse, Walked>,
+        email: String,
+        password: String,
+    ) -> juniper::FieldResult<SignupResponse> {
+        Ok(SignupResponse {
+            success: true,
+            errors: [].to_vec(),
+        })
+    }
+}
+
+pub struct SignupResponse {
+    success: bool,
+    errors: Vec<SignupResult>,
+}
+
+impl SignupResponseFields for SignupResponse {
+    fn field_success(
+        &self, 
+        _executor: &juniper::Executor<'_, Context>
+    ) -> juniper::FieldResult<&bool> {
+        Ok(&self.success)
+    }
+
+    fn field_errors(
+        &self, 
+        _executor: &juniper::Executor<'_, Context>
+    ) -> juniper::FieldResult<&Vec<SignupResult>> {
+        Ok(&self.errors)
+    }
+}
+
+fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://127.0.0.1:8080/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
+
+fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let res = data.execute(&st, &Context);
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .map_err(Error::from)
+    .and_then(|user| {
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(user))
+    })
+}
+
+fn main() -> io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    // Create Juniper schema
+    let schema = std::sync::Arc::new(Schema::new(Query, Mutation));
+
+    // Start http server
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
 }
